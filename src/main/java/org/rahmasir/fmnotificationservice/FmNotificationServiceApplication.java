91ae4e7754120b03db99c6fc3397210520b23df6
EnAnsari <rahmat.ansari.dev@gmail.com>
2025-08-16 16:23:38 +0330

    create: structure of project

---
+++ src/main/java/org/rahmasir/fmnotificationservice/FmNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rahmasir.fmnotificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmNotificationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/rahmasir/fmnotificationservice/FmNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rahmasir.fmnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
